df25db8505546e2515a6fec55e06ef8690bc2897
laerson <20879937+Laerson@users.noreply.github.com>
2023-04-14 01:37:52 -0300

    Add exception for when value of Fatura is negative

--- src/main/java/org/example/Fatura.java
+++ src/main/java/org/example/Fatura.java
@@ -8,6 +8,9 @@ public class Fatura {
     private String name;
 
     public Fatura(LocalDate date, int value, String name) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value can`t be negative");
+        }
         this.date = date;
         this.value = value;
         this.name = name;
