e7d5f10c8d73d911956eeb245ead076cce6f9349
laerson <20879937+Laerson@users.noreply.github.com>
2023-04-14 02:02:55 -0300

    Modify Boleto

--- src/main/java/org/example/Boleto.java
+++ src/main/java/org/example/Boleto.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Boleto {
+    String id;
+    LocalDate date;
+    int value;
+
+    public Boleto(String id, LocalDate date, int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("Value must be positive");
+        }
+        this.id = id;
+        this.date = date;
+        this.value = value;
+    }
 }
